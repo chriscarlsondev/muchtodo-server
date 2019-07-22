2fbb27b2067fbf76c6aead1b628a1ccefe041f9e
Chris Carlson <cecarlson@gmail.com>
2019-07-22 01:52:31 -0500

    removed check for bearer token

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
 const winston = require('winston');
 const tasksRouter = require('./tasks/tasks-router')
 const categoriesRouter = require('./categories/categories-router')
@@ -13,23 +13,17 @@ const app = express()
 
 app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
     skip: () => NODE_ENV === 'test',
-  }))
+}))
+  
+app.use(cors());
+// app.use(
+//     cors({
+//         origin: CLIENT_ORIGIN
+//     })
+// );
 
-app.use(cors())
 app.use(helmet())
 app.use(express.json());
-
-app.use(function validateBearerToken(req, res, next) {
-    const apiToken = process.env.API_TOKEN
-    const authToken = req.get('Authorization')
-  
-    if (!authToken || authToken.split(' ')[1] !== apiToken) {
-      logger.error(`Unauthorized request to path: ${req.path}`);
-      return res.status(401).json({ error: 'Unauthorized request' })
-    }
-    // move to the next middleware
-    next()
-})
   
 app.use('/api/tasks',tasksRouter)
 app.use('/api/categories',categoriesRouter)
--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: 'postgresql://dunder-mifflin@localhost/muchtodo'
+  DB_URL: 'postgresql://dunder-mifflin@localhost/muchtodo',
+    CORS_ORIGIN: '*'
   }
